32fd695a983f8c2c46e3d2ad5506f8dc11cabb07
Jnanaranjan Sahoo <111556500+jnana890@users.noreply.github.com>
2025-07-09 11:46:10 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from EC2 + Docker!"}
+
+
+@app.get("/home")
+def read_root():
+    return {"message": "welcome to home"}
